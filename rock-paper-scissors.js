5a275fcde8a690328a861ba42298088e1165155d
brennenmichael <kelyen@aol.com>
2022-02-20 15:45:41 -0500

    Added game function. Need to add handling for invalid input.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -11,16 +11,61 @@ function gameRound(playerSelection, computerSelection) {
     computerSelection = computerPlay();
     playerSelection = prompt("Rock, paper, scissors...").trim().toLowerCase();
     console.log(playerSelection, computerSelection);
+    if (playerSelection === "q".trimEnd().toLowerCase())
+        return;
     if (!choice.includes(playerSelection)) {
         alert("Invalid selection.")
     }
     if (choice.indexOf(computerSelection) === choice.indexOf(playerSelection)) {
         alert("It's a tie!")
+        computerScore = 0;
+        playerScore = 0;
     } else if (choice.indexOf(computerSelection) - choice.indexOf(playerSelection) == 1 || choice.indexOf(computerSelection) - choice.indexOf(playerSelection) == -2) {
         alert(`You chose ${playerSelection}, but the computer chose ${computerSelection}. Sorry!`)
+        computerScore = 1;
+        playerScore = 0;
     } else {
         alert(`The computer chose ${computerSelection}, and you chose ${playerSelection}. Good job!`)
+        computerScore = 0;
+        playerScore = 1;
     }
+    return [computerScore, playerScore];
 }
 
-gameRound();
+function game() {
+    alert("Rock Paper Scissors! Best out of five wins!");
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let rounds = 0; rounds < 5; rounds++) {
+        alert(`Round ${rounds + 1}!`);
+        let scores = gameRound();
+        computerScore += scores[0];
+        playerScore += scores[1];
+        console.log(`player: ${playerScore}, comp: ${computerScore}`);
+    }
+
+    if (playerScore > computerScore) {
+        alert("You win!")
+    }
+    else if (playerScore < computerScore) {
+        alert("The computer outsmarted you... this time.")
+    }
+    else {
+        while (playerScore == computerScore) {
+            alert("Tiebreaker!")
+            let scores = gameRound();
+            if (scores[0] > scores[1]) {
+                alert("Well fought, but the computer won.");
+                break;
+            } else if (scores [1] > scores [0]) {
+                alert("You emerge victorious!");
+                break;
+            } else {
+                alert("Another tie! Here we go again!")
+            } continue;
+        }
+    }
+    alert("That's all! Thanks for playing!")
+}
+
+game();
