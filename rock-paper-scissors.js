fbb6a5ac1cb13d47ce6f34d2fd273ab5d7ebf9a9
brennenmichael <kelyen@aol.com>
2022-02-28 17:01:12 -0500

    Began refactor of code to play better with HTML.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -6,21 +6,23 @@ function computerPlay() {
     return computerSelection;
 }
 
-function checkInput(input, array) {
+function isInArray(input, array) {
     if (!array.includes(input)) {
         return false;
     } return true;
 }
 
-function gameRound(playerSelection, computerSelection) {
+
+
+
+function playerResult() {
+    selection = playerPick();
+    console.log(`Selection is ${selection}`)
+}
+
+function roundResult(computerSelection, playerSelection) {
     computerSelection = computerPlay();
-    playerSelection = prompt("Rock, paper, scissors...").trim().toLowerCase();
-    console.log(playerSelection, computerSelection);
-    if (playerSelection === "q".trimEnd().toLowerCase())
-        return;
-    while (!checkInput(playerSelection, choice)){
-        playerSelection = prompt("Invalid input. Please try again.");
-    }
+    playerSelection = playerPick();
     if (choice.indexOf(computerSelection) === choice.indexOf(playerSelection)) {
         changeText('game-text', "It's a tie!");
         computerScore = 0;
@@ -37,17 +39,51 @@ function gameRound(playerSelection, computerSelection) {
     return [computerScore, playerScore];
 }
 
-function changeText(target, newtext) {
-    let elem = document.getElementById(target);
-    elem.textContent = newtext;
+
+async function gameRound() { //previously (playerSelection, computerSelection)
+    changeText('game-text', "Rock, paper, scissors...");
+    computerSelection = computerPlay();
+    playerSelection = await playerPick();
+    playerSelection = new Promise((resolve, reject) => {
+        resolve(playerPick());
+    })
+    Promise.all([computerSelection, playerSelection]).then((selections) => {
+        console.log(selections);
+    })
+    // let selections;
+    // selections = [computerSelection, playerSelection]
+    // while (typeof playerSelection === 'undefined') {
+    //     playerSelection = playerPick();
+    // }
+    // if (typeof playerSelection !== 'undefined') {
+    //     console.log(selections);
+    //     return selections;
+    // }
+    // console.log(selections);
 }
 
-function game() {
-    changeText('game-text', "Rock Paper Scissors! Best out of five wins! \n(Enter q at any time to quit.)")
+// Promise.all([promise1, promise2, promise3]).then((values) => {
+//     console.log(values);
+//   });
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+function changeText(target, newText) {
+    let elem = document.getElementById(target);
+    elem.textContent = newText;
+    //await sleep(2000)
+}
+
+
+async function game() {
+    changeText('game-text', "Rock Paper Scissors! Best out of five wins! \n(Enter q at any time to quit.)");
     let playerScore = 0;
     let computerScore = 0;
-    for (let rounds = 0; rounds < 5; rounds++) {
-        changeText('game-text', `Round ${rounds + 1}!`)
+    for (let rounds = 1; rounds <= 5; rounds++) {
+        await sleep (2000);
+        changeText('game-text', `Round ${rounds}!`);
         let scores = gameRound();
         if (scores === undefined) {
             return;
@@ -82,6 +118,49 @@ function game() {
 }
 
 document.addEventListener('keyup', e => {
-if (e.key === 'Enter') return game();
+    if (e.key === 'Enter') return game();
 })
 
+document.addEventListener('click', (e) => {
+    let element = e.target;
+    let pick;
+    if (element.tagName == "BUTTON") {
+        pick = `${element.innerText}`.toLowerCase();
+    }
+    console.log(pick)
+    let computerChoice = computerPlay()
+    console.log(`computer choice is ${computerChoice}`)
+    if (choice.includes(pick)) {
+        if (computerChoice === 'rock') return computerRock(pick);
+        if (computerChoice === 'paper') return computerPaper(pick);
+        if (computerChoice === 'scissors') return computerScissors(pick);
+    pick = "undefined" 
+    }
+}) 
+
+function computerRock(playerChoice) {
+    if (playerChoice === 'scissors') alert("You lose!");
+    else if (playerChoice === 'paper') alert("You win!")
+    else alert("Tie!")
+    playerChoice = 'undefined'
+    computerPlay()
+    return
+}
+
+function computerPaper(playerChoice) {
+    if (playerChoice === 'rock') alert("You lose!");
+    else if (playerChoice === 'scissors') alert("You win!")
+    else alert("Tie!")
+    playerChoice = 'undefined'
+    computerPlay()
+    return
+}
+
+function computerScissors(playerChoice) {
+    if (playerChoice === 'paper') alert("You lose!");
+    else if (playerChoice === 'rock') alert("You win!")
+    else alert("Tie!")
+    playerChoice = 'undefined'
+    computerPlay()
+    return
+}
