782303c02732a6ff7bfaf7e2c8ac7a149f715bc2
brennenmichael <kelyen@aol.com>
2022-03-01 20:54:34 -0500

    Removed unnecessary functions from .js.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -6,78 +6,14 @@ function computerPlay() {
     return computerSelection;
 }
 
-function roundResult(computerSelection, playerSelection) {
-    computerSelection = computerPlay();
-    playerSelection = playerPick();
-    if (choice.indexOf(computerSelection) === choice.indexOf(playerSelection)) {
-        changeText('game-text', "It's a tie!");
-        computerScore = 0;
-        playerScore = 0;
-    } else if (choice.indexOf(computerSelection) - choice.indexOf(playerSelection) == 1 || choice.indexOf(computerSelection) - choice.indexOf(playerSelection) == -2) {
-        changeText('game-text', `You chose ${playerSelection}, but the computer chose ${computerSelection}. Sorry!`);
-        computerScore = 1;
-        playerScore = 0;
-    } else {
-        changeText('game-text', `The computer chose ${computerSelection}, and you chose ${playerSelection}. Good job!`);
-        computerScore = 0;
-        playerScore = 1;
-    }
-    return [computerScore, playerScore];
-}
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
-
 function changeText(target, newText) {
     let elem = document.getElementById(target);
     elem.textContent = newText;
-    //await sleep(2000)
 }
 
 function addToScore(target) {
     let elem = document.querySelector(target)
     elem.textContent++;
-}
-
-async function game() {
-    changeText('game-text', "Rock Paper Scissors! Best out of five wins! \n(Enter q at any time to quit.)");
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let rounds = 1; rounds <= 5; rounds++) {
-        await sleep (2000);
-        changeText('game-text', `Round ${rounds}!`);
-        let scores = gameRound();
-        if (scores === undefined) {
-            return;
-        }
-        computerScore += scores[0];
-        playerScore += scores[1];
-        //console.log(`player: ${playerScore}, comp: ${computerScore}`);
-    }
-
-    if (playerScore > computerScore) {
-        changeText('game-text', 'You win!');
-    }
-    else if (playerScore < computerScore) {
-        changeText('game-text', 'The computer outsmarted you... this time.');
-    }
-    else {
-        while (playerScore == computerScore) {
-            changeText('game-text', 'Tiebreaker!');
-            let scores = gameRound();
-            if (scores[0] > scores[1]) {
-                changeText('game-text', 'Well fought, but the computer won.');
-                break;
-            } else if (scores [1] > scores [0]) {
-                changeText('game-text', 'You emerge victorious!');
-                break;
-            } else {
-                changeText('game-text', 'Another tie! Here we go again!');
-            } continue;
-        }
-    }
-    changeText('game-text', "That's all! Thanks for playing!");
 }
 
 document.addEventListener('keyup', e => {
